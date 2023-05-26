ae31fb05dafa25da7db60ec80aef657c773c87f9
bogotto <vladlengellervvv@gmail.com>
2023-05-26 12:47:55 +0300

    final2

--- src/generalGcd.js
+++ src/generalGcd.js
@@ -1,14 +1,14 @@
 const getDivisor = (a, b) => {
-    let one = a;
-    let two = b;
-    while (one !== two) {
-      if (one > two) {
-        one -= two;
-      } else {
-        two -= one;
-      }
+  let one = a;
+  let two = b;
+  while (one !== two) {
+    if (one > two) {
+      one -= two;
+    } else {
+      two -= one;
     }
-    return one;
-  };
-  
-  export default getDivisor;
+  }
+  return one;
+};
+
+export default getDivisor;
